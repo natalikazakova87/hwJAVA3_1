85956aca5ba9c6ef04f7c3301408c9b29cf875f9
natalikazakova <natalikazakova87@gmail.com>
2023-06-10 11:45:48 +0700

    initial commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,9 +1,9 @@
 public class BonusMilesService {
     public int calculate(int price) {
         int result;
-        if (price > 20){
+        if (price > 20) {
             result = price / 20;
-        } else{
+        } else {
             result = 0;
         }
         return result;
